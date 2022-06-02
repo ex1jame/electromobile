46fb35fab9cfad1034aed758a227adf3e8da8935
ex1jame <81546043+ex1jame@users.noreply.github.com>
2022-06-03 01:28:33 +0600

    fix

--- src/components/TestDrive.js
+++ src/components/TestDrive.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import '../style/TestDrive.css'
 import React from 'react'
 
@@ -85,16 +85,7 @@ const TestDrive = () => {
             </section>
         </div>
     )
-=======
-import React from 'react'
 
-const TestDrive = () => {
-	return (
-		<div style={{backgroundColor : "#000", height: "100vh"}}>
-
-		</div>
-	)
->>>>>>> f05341b78b9fe9cf40e9cfdfbebd7c1f42ccb9a3
 }
 
 export default TestDrive
